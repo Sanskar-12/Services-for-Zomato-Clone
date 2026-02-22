f0a74e3114c9d1afca098bc18672b60c550ad847
Sanskar-12 <sanskarv2004@gmail.com>
2026-02-22 09:53:43 +0530

    Feature: Created a middleware for try catch and tested the login api

--- auth/src/controllers/auth.ts
+++ auth/src/controllers/auth.ts
@@ -1,41 +1,34 @@
 import { Request, Response } from "express";
 import { User } from "../models/User.js";
 import jwt from "jsonwebtoken";
+import TryCatch from "../middlewares/trycatch.js";
 
-export const loginUser = async (req: Request, res: Response) => {
-  try {
-    const { name, email, picture } = req.body;
+export const loginUser = TryCatch(async (req, res) => {
+  const { name, email, picture } = req.body;
 
-    let user = await User.findOne({ email });
+  let user = await User.findOne({ email });
 
-    if (!user) {
-      user = await User.create({
-        name,
-        email,
-        image: picture,
-      });
-    }
-
-    const token = jwt.sign(
-      {
-        _id: user?._id,
-      },
-      process.env.JWT_SECRET as string,
-      {
-        expiresIn: "15d",
-      },
-    );
-
-    return res.status(200).json({
-      message: "Logged In Success",
-      token,
-      user,
-    });
-  } catch (error) {
-    console.log("Error in loginUser Controller: ", error);
-    return res.status(500).json({
-      success: false,
-      message: `SignUp loginUser error: ${error}`,
+  if (!user) {
+    user = await User.create({
+      name,
+      email,
+      image: picture,
     });
   }
-};
+
+  const token = jwt.sign(
+    {
+      _id: user?._id,
+    },
+    process.env.JWT_SECRET as string,
+    {
+      expiresIn: "15d",
+    },
+  );
+
+  return res.status(200).json({
+    message: "Logged In Success",
+    token,
+    user,
+  });
+});
