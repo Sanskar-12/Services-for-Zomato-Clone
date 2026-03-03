44da21c64a71da11ce46ecd061b1aed794c2378d
Sanskar-12 <sanskarv2004@gmail.com>
2026-03-03 12:14:24 +0530

    Feature: Added cors in backend

--- auth/dist/index.js
+++ auth/dist/index.js
@@ -8,7 +8,8 @@ connectDb();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: "*",
+    origin: "http://localhost:5173",
+    credentials: true,
 }));
 app.use("/api/auth", authRouter);
 const PORT = process.env.PORT;
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -12,7 +12,8 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
+    credentials: true,
   }),
 );
 
