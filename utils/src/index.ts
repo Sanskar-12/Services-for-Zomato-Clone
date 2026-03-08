185f8210bab027d55da677b27644514ffc40222b
Sanskar-12 <sanskarv2004@gmail.com>
2026-03-08 12:47:30 +0530

    Feature: Created Utils service for cloudinary and payments

---
+++ utils/src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Utils Service is running on PORT ${PORT}`);
+});
