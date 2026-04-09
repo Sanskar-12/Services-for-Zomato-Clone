163e36607c30312ff0f7ef84569a23ae140f9b0b
Sanskar-12 <sanskarv2004@gmail.com>
2026-04-09 13:15:31 +0530

    Feature: Added a route for creating rider profile

--- rider/dist/routes/rider.js
+++ rider/dist/routes/rider.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
-import { fetchMyProfile, toggleRiderAvailability, } from "../controllers/rider.js";
+import { addRiderProfile, fetchMyProfile, toggleRiderAvailability, } from "../controllers/rider.js";
+import uploadFile from "../middlewares/multer.js";
 const router = express.Router();
+router.post("/new", isAuth, uploadFile, addRiderProfile);
 router.get("/myProfile", isAuth, fetchMyProfile);
 router.patch("/toggle", isAuth, toggleRiderAvailability);
 export default router;
--- rider/src/routes/rider.ts
+++ rider/src/routes/rider.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
 import {
+  addRiderProfile,
   fetchMyProfile,
   toggleRiderAvailability,
 } from "../controllers/rider.js";
+import uploadFile from "../middlewares/multer.js";
 
 const router = express.Router();
 
+router.post("/new", isAuth, uploadFile, addRiderProfile);
 router.get("/myProfile", isAuth, fetchMyProfile);
 router.patch("/toggle", isAuth, toggleRiderAvailability);
 
