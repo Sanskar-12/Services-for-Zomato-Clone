6c1c561bef981296547ebe4f7243832be7976014
Sanskar-12 <sanskarv2004@gmail.com>
2026-04-14 09:20:59 +0530

    Feature: Intialise admin service

---
+++ admin/dist/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Admin Service is running on PORT ${PORT}`);
+});
---
+++ admin/src/middlewares/isAuth.ts
@@ -0,0 +1,87 @@
+import { NextFunction, Response, Request } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+export interface IUser {
+  _id: string;
+  name: string;
+  email: string;
+  image: string;
+  role: string;
+  restaurantId: string;
+}
+
+export interface AuthenticatedRequest extends Request {
+  user?: IUser | null;
+}
+
+export const isAuth = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({
+        message: "Please Login - No auth header",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Please Login - Token missing",
+      });
+    }
+
+    const decodedValue = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string,
+    ) as JwtPayload;
+
+    if (!decodedValue || !decodedValue.user) {
+      return res.status(401).json({
+        message: "Invalid token",
+      });
+    }
+
+    req.user = decodedValue.user;
+
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      message: "Please Login - Jwt error",
+    });
+  }
+};
+
+export const isAdmin = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({
+        success: false,
+        message: "Please Login",
+      });
+    }
+
+    if (req.user.role !== "admin") {
+      return res.status(403).json({
+        success: false,
+        message: "Access denied",
+      });
+    }
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Please Login",
+    });
+  }
+};
