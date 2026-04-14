e92ba7893a65520d6feb8e5eede6582adc4a2fea
Sanskar-12 <sanskarv2004@gmail.com>
2026-04-14 09:34:06 +0530

    Feature: Made apis for verify and getting pending forms of restautants and riders

--- admin/src/index.ts
+++ admin/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import adminRoutes from "./routes/admin.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/v1/admin", adminRoutes);
 
 const PORT = process.env.PORT;
 
